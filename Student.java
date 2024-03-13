596b7b8604dabbde54ed3c3add325d0bf48011ec
Aisha116 <01fe22bca116@kletech.ac.in>
2024-03-13 20:12:41 +0530

    Code to display student's Name & Age by accepting DOB as parameter (DD-MM-YYYY or YYYY-MM-DD)

---
+++ Student.java
@@ -0,0 +1,31 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+class Student{
+    private String name;
+    private LocalDate dateofbirth;
+
+    public Student(String name ,String dateofbirth)
+    {
+        this.name=name;
+        DateTimeFormatter formatter =DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        this.dateofbirth =LocalDate.parse(dateofbirth,formatter);
+    }
+    public void displaystudentname()
+    {
+        System.out.println("Student Name: " + name);
+    }
+    public void displayage()
+    {
+        LocalDate currentDate = LocalDate.now();
+        long age  = ChronoUnit.YEARS.between(this.dateofbirth ,currentDate );
+        System.out.println("Age of Student is: " + age + " Years");
+    }
+    public static void main(String[] args) {
+        Student stu = new Student("Aisha","2003-12-27");
+        stu.displayage();
+        stu.displaystudentname();
+
+    }
+}
